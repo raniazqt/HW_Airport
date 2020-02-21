4d6119f595dc74adee72d20bd5265f5dca109958
Bruce <brucetauro@gmail.com>
2020-02-21 16:01:11 +0400

    More adjustments

--- src/com/hw/airport/sGUI/DisplayWelcomeScreen.java
+++ src/com/hw/airport/sGUI/DisplayWelcomeScreen.java
@@ -1,19 +1,16 @@
 package com.hw.airport.sGUI;
 
 /**
- * @author Simon Grange, Safa Al Ameri,
+ * @author Simon Grange, Safa Al Ameri, Bruce Tauro
  */
 public class DisplayWelcomeScreen extends javax.swing.JFrame {
 
-	/**
-	 * Creates new form Competitor
-	 */
+	private javax.swing.JButton jButton1;
+	private javax.swing.JPanel jPanel1;
+
 	public DisplayWelcomeScreen() {
 		jPanel1 = new javax.swing.JPanel();
-		
-		
 		jButton1 = new javax.swing.JButton();
-		
 
 		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -25,13 +22,12 @@ public class DisplayWelcomeScreen extends javax.swing.JFrame {
 		jButton1.setText("Check In");
 		jButton1.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				jButton6ActionPerformed(evt);
+				jButton1ActionPerformed(evt);
 			}
 		});
 		jPanel1.add(jButton1);
 		jButton1.setBounds(130, 100, 130, 30);
 
-	
 		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
 		getContentPane().setLayout(layout);
 		layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addComponent(
@@ -42,34 +38,13 @@ public class DisplayWelcomeScreen extends javax.swing.JFrame {
 		pack();
 	}
 
-	private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {
-		// GEN-FIRST:event_jButton6ActionPerformed
-		// Handling code here for the ShortDetails object:
+	private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+
 
 		DisplaySearchScreen obj = new DisplaySearchScreen();
 		obj.setVisible(true);
 		this.setVisible(false);
 	}
-	// GEN-LAST:event_jButton6ActionPerformed
-	/*
-	 * 
-	 * 
-	 * private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) { //
-	 * GEN-FIRST:event_jButton5ActionPerformed // Handling code here for the
-	 * FullDetails object:
-	 * 
-	 * DisplayPaymentWindow obj = new DisplayPaymentWindow(); obj.setVisible(true);
-	 * this.setVisible(false); } // GEN-LAST:event_jButton5ActionPerformed
-	 * 
-	 * 
-	 * 
-	 * private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) { //
-	 * GEN-FIRST:event_jButton7ActionPerformed // Handling code here for the Manager
-	 * object:
-	 * 
-	 * Manager obj = new Manager(); obj.setVisible(true); this.setVisible(false); }
-	 * // GEN-LAST:event_jButton7ActionPerformed
-	 */
 
 	/**
 	 * @param args the command line arguments
@@ -84,13 +59,4 @@ public class DisplayWelcomeScreen extends javax.swing.JFrame {
 		});
 	}
 
-	// Variables declaration - do not modify
-	// GEN-BEGIN:variables
-
-	private javax.swing.JButton jButton1;
-	private javax.swing.JButton jButton5;
-	private javax.swing.JButton jButton7;
-	private javax.swing.JPanel jPanel1;
-	// End of variables declaration
-	// GEN-END:variables
 }
