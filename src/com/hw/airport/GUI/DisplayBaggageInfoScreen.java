e3824b41b7d8ba68208a2e4e58a53b5ffeb03c36
Bruce <brucetauro@gmail.com>
2020-02-25 15:59:50 +0400

    Verbose variables

--- src/com/hw/airport/GUI/DisplayBaggageInfoScreen.java
+++ src/com/hw/airport/GUI/DisplayBaggageInfoScreen.java
@@ -23,7 +23,7 @@ public class DisplayBaggageInfoScreen extends javax.swing.JFrame {
 	private javax.swing.JButton Calc;
 
 	
-	private javax.swing.JLabel jLabel2;
+	private javax.swing.JLabel tleLbl;
 	private javax.swing.JLabel lenLbl;
 	private javax.swing.JLabel weiLbl;
 	private javax.swing.JLabel heiLbl;
@@ -31,9 +31,9 @@ public class DisplayBaggageInfoScreen extends javax.swing.JFrame {
 	private javax.swing.JLabel widLbl;
 	
 	
-	private javax.swing.JPanel jPanel1;
-	private javax.swing.JPanel jPanel2;
-	private javax.swing.JPanel jPanel3;
+	private javax.swing.JPanel outPnl;
+	private javax.swing.JPanel midPnl;
+	private javax.swing.JPanel inPnl;
 	
 	private static final long serialVersionUID = 1L;
 
@@ -42,18 +42,19 @@ public class DisplayBaggageInfoScreen extends javax.swing.JFrame {
 	 */
 	public DisplayBaggageInfoScreen() {
 
-		jPanel1 = new javax.swing.JPanel();
-		jPanel2 = new javax.swing.JPanel();
-		jPanel3 = new javax.swing.JPanel();
+		outPnl = new javax.swing.JPanel();
+		midPnl = new javax.swing.JPanel();
+		inPnl = new javax.swing.JPanel();
 
 		
-		jLabel2 = new javax.swing.JLabel();
+		tleLbl = new javax.swing.JLabel();
 
 		lenLbl = new javax.swing.JLabel();
 		weiLbl = new javax.swing.JLabel();
 		heiLbl = new javax.swing.JLabel();
-		feeLbl = new javax.swing.JLabel();
 		widLbl = new javax.swing.JLabel();
+		feeLbl = new javax.swing.JLabel();
+		
 		
 
 		Length = new javax.swing.JTextField();
@@ -69,13 +70,13 @@ public class DisplayBaggageInfoScreen extends javax.swing.JFrame {
 		
 		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-		jPanel1.setLayout(null);
+		outPnl.setLayout(null);
 
-		jPanel2.setLayout(null);
+		midPnl.setLayout(null);
 
 		// Main panel pale blue background
-		jPanel3.setBackground(new java.awt.Color(240, 255, 255));
-		jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder(null,
+		inPnl.setBackground(new java.awt.Color(240, 255, 255));
+		inPnl.setBorder(javax.swing.BorderFactory.createTitledBorder(null,
 				"Please complete the information about your baggage", 0, 0, new java.awt.Font("Helvetica", 1, 14))); // NOI18N
 
 		lenLbl.setFont(new java.awt.Font("Helvetica", 1, 14)); // NOI18N
@@ -119,38 +120,38 @@ public class DisplayBaggageInfoScreen extends javax.swing.JFrame {
 
 	
 
-		javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-		jPanel3.setLayout(jPanel3Layout);
-		jPanel3Layout.setHorizontalGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-				.addGroup(jPanel3Layout.createSequentialGroup().addGap(191, 191, 191).addGap(62, 62, 62)
+		javax.swing.GroupLayout inPnlLayout = new javax.swing.GroupLayout(inPnl);
+		inPnl.setLayout(inPnlLayout);
+		inPnlLayout.setHorizontalGroup(inPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+				.addGroup(inPnlLayout.createSequentialGroup().addGap(191, 191, 191).addGap(62, 62, 62)
 						.addGap(0, 0, Short.MAX_VALUE))
-				.addGroup(jPanel3Layout.createSequentialGroup().addContainerGap()
-						.addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-								.addGroup(jPanel3Layout.createSequentialGroup().addGroup(jPanel3Layout
+				.addGroup(inPnlLayout.createSequentialGroup().addContainerGap()
+						.addGroup(inPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+								.addGroup(inPnlLayout.createSequentialGroup().addGroup(inPnlLayout
 										.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-										.addGroup(jPanel3Layout.createSequentialGroup().addComponent(feeLbl)
+										.addGroup(inPnlLayout.createSequentialGroup().addComponent(feeLbl)
 												.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED,
 														javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 												.addComponent(FeesToPay, javax.swing.GroupLayout.PREFERRED_SIZE, 200,
 														javax.swing.GroupLayout.PREFERRED_SIZE))
-										.addGroup(jPanel3Layout.createSequentialGroup()
+										.addGroup(inPnlLayout.createSequentialGroup()
 												.addComponent(Calc, javax.swing.GroupLayout.PREFERRED_SIZE, 77,
 														javax.swing.GroupLayout.PREFERRED_SIZE)
 												.addGap(173, 173, 173))
-										.addGroup(jPanel3Layout.createSequentialGroup()
+										.addGroup(inPnlLayout.createSequentialGroup()
 												.addComponent(widLbl, javax.swing.GroupLayout.PREFERRED_SIZE, 49,
 														javax.swing.GroupLayout.PREFERRED_SIZE)
 												.addGap(59, 59, 59)
 												.addComponent(Width, javax.swing.GroupLayout.PREFERRED_SIZE, 200,
 														javax.swing.GroupLayout.PREFERRED_SIZE)))
-										.addGroup(jPanel3Layout
+										.addGroup(inPnlLayout
 												.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-												.addGroup(jPanel3Layout.createSequentialGroup().addGap(106, 106, 106)
+												.addGroup(inPnlLayout.createSequentialGroup().addGap(106, 106, 106)
 														
 														.addPreferredGap(
 																javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43,
 																Short.MAX_VALUE)
-														.addGroup(jPanel3Layout
+														.addGroup(inPnlLayout
 																.createParallelGroup(
 																		javax.swing.GroupLayout.Alignment.LEADING)
 																.addComponent(Category,
@@ -161,7 +162,7 @@ public class DisplayBaggageInfoScreen extends javax.swing.JFrame {
 																		javax.swing.GroupLayout.Alignment.TRAILING,
 																		javax.swing.GroupLayout.PREFERRED_SIZE, 200,
 																		javax.swing.GroupLayout.PREFERRED_SIZE)))
-												.addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout
+												.addGroup(javax.swing.GroupLayout.Alignment.TRAILING, inPnlLayout
 														.createSequentialGroup()
 														.addPreferredGap(
 																javax.swing.LayoutStyle.ComponentPlacement.RELATED,
@@ -169,18 +170,18 @@ public class DisplayBaggageInfoScreen extends javax.swing.JFrame {
 														.addComponent(Submit, javax.swing.GroupLayout.PREFERRED_SIZE,
 																77, javax.swing.GroupLayout.PREFERRED_SIZE)
 														.addGap(52, 52, 52))))
-								.addGroup(jPanel3Layout.createSequentialGroup().addGap(33).addComponent(lenLbl)
+								.addGroup(inPnlLayout.createSequentialGroup().addGap(33).addComponent(lenLbl)
 										.addGap(63, 63, 63)
 										.addComponent(
 												Length, javax.swing.GroupLayout.PREFERRED_SIZE, 200,
 												javax.swing.GroupLayout.PREFERRED_SIZE)
 										.addGap(106, 106, 106)
-										.addGroup(jPanel3Layout
+										.addGroup(inPnlLayout
 												.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-												.addGroup(jPanel3Layout
+												.addGroup(inPnlLayout
 														.createSequentialGroup().addComponent(heiLbl)
 														.addGap(0, 0, Short.MAX_VALUE))
-												.addGroup(jPanel3Layout.createSequentialGroup().addComponent(weiLbl)
+												.addGroup(inPnlLayout.createSequentialGroup().addComponent(weiLbl)
 														.addPreferredGap(
 																javax.swing.LayoutStyle.ComponentPlacement.RELATED,
 																javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -189,16 +190,16 @@ public class DisplayBaggageInfoScreen extends javax.swing.JFrame {
 						.addContainerGap()));
 		
 		
-		jPanel3Layout
-				.setVerticalGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-						.addGroup(jPanel3Layout.createSequentialGroup().addGroup(
-								jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-										.addGroup(jPanel3Layout.createSequentialGroup().addGap(23, 23, 23)))
-								.addGroup(jPanel3Layout
+		inPnlLayout
+				.setVerticalGroup(inPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+						.addGroup(inPnlLayout.createSequentialGroup().addGroup(
+								inPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+										.addGroup(inPnlLayout.createSequentialGroup().addGap(23, 23, 23)))
+								.addGroup(inPnlLayout
 										.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
 										.addGroup(
-												jPanel3Layout.createSequentialGroup().addGap(0, 0, Short.MAX_VALUE)
-														.addGroup(jPanel3Layout
+												inPnlLayout.createSequentialGroup().addGap(0, 0, Short.MAX_VALUE)
+														.addGroup(inPnlLayout
 																.createParallelGroup(
 																		javax.swing.GroupLayout.Alignment.BASELINE)
 																.addComponent(feeLbl).addComponent(FeesToPay,
@@ -208,7 +209,7 @@ public class DisplayBaggageInfoScreen extends javax.swing.JFrame {
 																		javax.swing.GroupLayout.PREFERRED_SIZE, 30,
 																		javax.swing.GroupLayout.PREFERRED_SIZE))
 														.addGap(81, 81, 81))
-										.addGroup(jPanel3Layout.createSequentialGroup().addGroup(jPanel3Layout
+										.addGroup(inPnlLayout.createSequentialGroup().addGroup(inPnlLayout
 												.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
 												.addComponent(lenLbl).addComponent(
 														Length, javax.swing.GroupLayout.PREFERRED_SIZE, 32,
@@ -216,19 +217,19 @@ public class DisplayBaggageInfoScreen extends javax.swing.JFrame {
 												.addComponent(weiLbl).addComponent(Weight,
 														javax.swing.GroupLayout.PREFERRED_SIZE, 32,
 														javax.swing.GroupLayout.PREFERRED_SIZE))
-												.addGroup(jPanel3Layout
+												.addGroup(inPnlLayout
 														.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-														.addGroup(jPanel3Layout.createSequentialGroup()
+														.addGroup(inPnlLayout.createSequentialGroup()
 																.addGap(31, 31, 31)
-																.addGroup(jPanel3Layout.createParallelGroup(
+																.addGroup(inPnlLayout.createParallelGroup(
 																		javax.swing.GroupLayout.Alignment.BASELINE)
 																		.addComponent(widLbl).addComponent(Width,
 																				javax.swing.GroupLayout.PREFERRED_SIZE,
 																				32,
 																				javax.swing.GroupLayout.PREFERRED_SIZE)))
-														.addGroup(jPanel3Layout.createSequentialGroup()
+														.addGroup(inPnlLayout.createSequentialGroup()
 																.addGap(20, 20, 20)
-																.addGroup(jPanel3Layout.createParallelGroup(
+																.addGroup(inPnlLayout.createParallelGroup(
 																		javax.swing.GroupLayout.Alignment.BASELINE)
 																		.addComponent(heiLbl).addComponent(Height,
 																				javax.swing.GroupLayout.PREFERRED_SIZE,
@@ -236,30 +237,30 @@ public class DisplayBaggageInfoScreen extends javax.swing.JFrame {
 																				javax.swing.GroupLayout.PREFERRED_SIZE))))
 												.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED,
 														144, Short.MAX_VALUE)))
-								.addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+								.addGroup(inPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
 										.addComponent(Submit, javax.swing.GroupLayout.DEFAULT_SIZE, 39, Short.MAX_VALUE)
 										.addComponent(Calc, javax.swing.GroupLayout.DEFAULT_SIZE, 39, Short.MAX_VALUE)
 										)
 								.addContainerGap()));
 
-		jPanel2.add(jPanel3);
-		jPanel3.setBounds(30, 80, 760, 360);
+		midPnl.add(inPnl);
+		inPnl.setBounds(30, 80, 760, 360);
 
-		jLabel2.setBackground(new java.awt.Color(240, 255, 255));
-		jLabel2.setFont(new java.awt.Font("Helvetica", 3, 16)); // NOI18N
-		jLabel2.setText("Baggage Check In Information");
-		jPanel2.add(jLabel2);
-		jLabel2.setBounds(0, 10, 360, 60);
+		tleLbl.setBackground(new java.awt.Color(240, 255, 255));
+		tleLbl.setFont(new java.awt.Font("Helvetica", 3, 16)); // NOI18N
+		tleLbl.setText("Baggage Check In Information");
+		midPnl.add(tleLbl);
+		tleLbl.setBounds(0, 10, 360, 60);
 		
-		jPanel1.add(jPanel2);
-		jPanel2.setBounds(0, 0, 810, 500);
+		outPnl.add(midPnl);
+		midPnl.setBounds(0, 0, 810, 500);
 
 		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
 		getContentPane().setLayout(layout);
 		layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-				.addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 811, Short.MAX_VALUE));
+				.addComponent(outPnl, javax.swing.GroupLayout.DEFAULT_SIZE, 811, Short.MAX_VALUE));
 		layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-				.addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 499, Short.MAX_VALUE));
+				.addComponent(outPnl, javax.swing.GroupLayout.DEFAULT_SIZE, 499, Short.MAX_VALUE));
 
 		pack();
 	}// </editor-fold>//GEN-END:initComponents
